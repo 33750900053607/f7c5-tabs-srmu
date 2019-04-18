2a03fb0ca47b15c04f3c4e06efbd0b30595e1ca8
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-04-18 10:17:21 +0900

    time wheel

--- src/js/app.js
+++ src/js/app.js
@@ -185,7 +185,15 @@ var pickerDescribe = app.picker.create({
         textAlign: 'left',
         values: ('00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30').split(' ')
       },
-      // Divider
+    // Hours
+    {
+      values: (function () {
+        var arr = [];
+        for (var i = 0; i <= 28; i++) { arr.push(i); }
+          return arr;
+      })(),
+    },
+    // Divider
     {
       divider: true,
       content: ':'
